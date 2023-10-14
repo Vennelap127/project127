23ecdee75e1f9c22c74e465b7c3a298c4b16063f
pvenn <pvenn@LAPTOP-08UUHQ0J>
2023-10-14 19:14:00 +0530

    first commit

---
+++ multiple/src/com/tnsif/multiple/Multiple.java
@@ -0,0 +1,13 @@
+package com.tnsif.multiple;
+
+public class Multiple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,five_m;//1
+		a=22;//2 static initialization
+		five_m=5*a;//dynamic
+		System.out.println("The fifth multiple of 22 is" + five_m);
+	}
+
+}
