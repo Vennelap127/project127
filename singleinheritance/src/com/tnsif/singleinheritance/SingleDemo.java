9f6ce9f66477fc8164e0548a2e804db4497416cd
pvenn <pvenn@LAPTOP-08UUHQ0J>
2023-10-14 19:47:47 +0530

    first commit

---
+++ singleinheritance/src/com/tnsif/singleinheritance/SingleDemo.java
@@ -0,0 +1,11 @@
+package com.tnsif.singleinheritance;
+
+public class SingleDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  Student st=new Student("vennela",3782689607,"chennai",9088765343);
+  System.out.println(st);
+	}
+
+}
