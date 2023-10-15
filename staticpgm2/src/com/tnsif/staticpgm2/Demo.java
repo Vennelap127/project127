13e5f30f20e3523799607922eed308427d2d73f2
pvenn <pvenn@LAPTOP-08UUHQ0J>
2023-10-15 15:03:24 +0530

    first commit

---
+++ staticpgm2/src/com/tnsif/staticpgm2/Demo.java
@@ -0,0 +1,14 @@
+package com.tnsif.staticpgm2;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Myclass.display();//static method
+		Myclass obb1=new Myclass();
+		System.out.println("obb1");
+		Myclass obb2=new Myclass();
+		System.out.println("obb2");
+	}
+
+}
---
+++ staticpgm2/src/com/tnsif/staticpgm2/Myclass.java
@@ -0,0 +1,25 @@
+package com.tnsif.staticpgm2;
+
+public class Myclass {
+	private int section;//instance variable
+	private static int srno;//static variable
+	static {
+		System.out.println("---staticblock---");
+		srno=1000;
+	}
+   //default constructor
+	Myclass(){
+		System.out.println("---default constructor---");
+		srno++;
+		section++;
+	}
+	//static method
+	static void display() {
+		//System.out.println("section"+section);
+		System.out.println("serialno"+srno);
+	}
+	@Override
+	public String toString() {
+		return "Myclass [section=" +section);
+}
+}
