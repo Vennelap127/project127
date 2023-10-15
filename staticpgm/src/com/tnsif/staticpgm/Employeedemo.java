efe9d978426416ea716ee59ef8d4f26b78f2f6cb
pvenn <pvenn@LAPTOP-08UUHQ0J>
2023-10-15 14:59:10 +0530

    first commit

---
+++ staticpgm/src/com/tnsif/staticpgm/Employeedemo.java
@@ -0,0 +1,12 @@
+package com.tnsif.staticpgm;
+
+public class Employeedemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee ob=new Employee("venni",1214);
+		System.out.println(ob);
+		Employee companyName;//accessing the static variable using calss name
+	}
+
+}
