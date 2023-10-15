92788193c6d857a04dea739b5ea3a8eb669bd79f
pvenn <pvenn@LAPTOP-08UUHQ0J>
2023-10-15 11:27:15 +0530

    first commit

---
+++ multipleinherit/src/com/tnsif/multipleinherit/MultilevelDemo.java
@@ -0,0 +1,12 @@
+package com.tnsif.multipleinherit;
+
+public class MultilevelDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		City c=new City();
+		System.out.println(c);
+
+	}
+
+}
---
+++ multipleinherit/src/com/tnsif/multipleinherit/State.java
@@ -0,0 +1,14 @@
+package com.tnsif.multipleinherit;
+
+public class State extends Country{
+	private String Statename;
+
+	public String getStatename() {
+		return Statename;
+	}
+
+	public void setStatename(String statename) {
+		Statename = statename;
+	}
+
+}
