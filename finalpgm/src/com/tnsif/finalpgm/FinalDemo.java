8705372770366936ab2830aa061ee2e090e42b4a
pvenn <pvenn@LAPTOP-08UUHQ0J>
2023-10-15 14:49:53 +0530

    first commit

---
+++ finalpgm/src/com/tnsif/finalpgm/FinalDemo.java
@@ -0,0 +1,14 @@
+package com.tnsif.finalpgm;
+
+public class FinalDemo {
+  int a=20;
+  void display() {
+	  System.out.println("the value od a is" +a);
+  }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        FinalDemo fd=new FinalDemo();
+        fd.dispaly();
+	}
+
+}
